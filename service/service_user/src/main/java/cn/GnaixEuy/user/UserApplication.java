6bb07e7d46b7f5dae21dbdb90203db6b5ec9dd68
GnaixEuy <sgghcj@live.com>
2023-02-23 18:40:57 +0800

    :beer: 接入日志接入mysql

---
+++ service/service_user/src/main/java/cn/GnaixEuy/user/UserApplication.java
@@ -0,0 +1,23 @@
+package cn.GnaixEuy.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <img src="http://blog.gnaixeuy.cn/wp-content/uploads/2022/09/倒闭.png"/>
+ *
+ * <p>项目： media-v2 </p>
+ * 创建日期： 2023/2/23
+ *
+ * @author GnaixEuy
+ * @version 1.0.0
+ * @see <a href="https://github.com/GnaixEuy"> GnaixEuy的GitHub </a>
+ */
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
